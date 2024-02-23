0d6b360ae31b26a8f0aa6a69c2e188be181a3cc1
SirKnightFarron <marvtrimpop@gmail.com>
2024-02-23 06:14:38 +0100

    Add permission request and styles to Landing Screen

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,40 +1,89 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import BaseScreen from "../components/BaseScreen";
-import { LandingScreenProps } from "../types";
+import {
+    PermissionsAndroid,
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
 import Content from "../components/Content";
 import BaseButton from "../components/BaseButton";
+import BaseScreen from "../components/BaseScreen";
+import { LandingScreenProps } from "../types";
+import { useEffect } from "react";
+import { Colors } from "../constants";
 
-
-const LandingScreen = ({ route, navigation }: LandingScreenProps) => {
-    const handleButtonPress = () => {
-        navigation.navigate('Devices');
+const LandingScreen = ({ route, navigation, style }: LandingScreenProps) => {
+    const infoText = `Mit dieser App können Sie Bluetooth Geräte in ihrer Nähe Finden und konfigurieren. Tippen Sie dafür auf den gesuchten Eintrag in der Liste auf der Folgenden Seite und beginnen Sie mit der Konfiguration.`
+    const handleNextButtonPress = () => {
+        navigation.navigate('Devices')
     }
+    // first ask for permissions otherwise -> crash
+    useEffect(() => {
+        if (Platform.OS === 'android' && Platform.Version >= 23) {
+            PermissionsAndroid.check(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            ).then(result => {
+                if (result) {
+                    console.log('Permission like 200 OK');
+                } else {
+                    PermissionsAndroid.requestMultiple([
+                        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+                        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+                        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                    ]).then(result => {
+                        if (result) {
+                            console.log('User accepted!');
+                        } else {
+                            console.log('User refused!');
+                        }
+                    });
+                }
+            });
+        }
+    })
 
     return (
-        <BaseScreen route={route} style={styles.baseStyles}>
-            <View>
-                <Text>Heading</Text>
-            </View>
+        <BaseScreen route={route}>
             <Content>
-                <Text>Das hier soll man sehen</Text>
+                <View style={styles.infoBoxWrapper}>
+                    <Text style={styles.infoBox}>
+                        {infoText}
+                    </Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <BaseButton
+                        buttonText="Weiter"
+                        hitSlop={50}
+                        onPress={handleNextButtonPress}>
+                    </BaseButton>
+                </View>
             </Content>
-            <View>
-                <Text>Hier dann Buttons</Text>
-                <BaseButton
-                    buttonText="Search BT Devices"
-                    hitSlop={50} 
-                    onPress={handleButtonPress} 
-                ></BaseButton>
-            </View>
         </BaseScreen>
     );
 }
 
 const styles = StyleSheet.create({
-    baseStyles: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
+    content: {
+        height: '85%',
+        width: '100%',
+    },
+    buttonContainer: {
+        width: '100%',
+        paddingTop: 20,
+        alignSelf: 'flex-end'
+    },
+    infoBox: {
+        borderWidth: 1,
+        borderColor: Colors.Grey,
+        color: Colors.Grey,
+        backgroundColor: Colors.Background,
+        padding: 12,
+        fontSize: 16,
+        lineHeight: 18
+    },
+    infoBoxWrapper: {
+        paddingTop: 120,
+        height: '85%',
     }
 });
 
