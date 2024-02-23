8f755e9ad18a6927cbae34b9ff68f4385eb92627
SirKnightFarron <marvtrimpop@gmail.com>
2024-02-23 06:15:59 +0100

    Add styles, ble functionality and typing to Device List Item

--- src/components/DeviceListItem.tsx
+++ src/components/DeviceListItem.tsx
@@ -1,25 +1,66 @@
-import { Pressable, Text, View } from "react-native";
+import {
+    Pressable,
+    StyleSheet, Text, View
+} from "react-native";
+import { Peripheral } from "react-native-ble-manager";
+import { Colors } from "../constants";
+import React, { memo } from "react";
+
 
 type DeviceListItemProps = {
-    info: {
-        deviceName: String;
-        rssid: String;
-    };
-    style: {};
-    onPress: () => void;
+    selected: boolean;
+    item: Peripheral;
+    onPress: (item: Peripheral) => void;
 }
 
-const DeviceListItem = ({ info, style, onPress }: DeviceListItemProps) => {
+/**
+ * List Item for the devices Screen containing relevant information
+ */
+const DeviceListItem = ({ onPress, item, selected }: DeviceListItemProps) => {
     return (
         <Pressable
-            style={style}
-            onPress={onPress}>
+            hitSlop={50}
+            onPress={() => onPress(item)}
+            style={
+                [styles.devicePressable, selected && styles.selectedItem]
+            }>
             <View>
-                <Text>{info.deviceName}</Text>
-                <Text>{info.rssid}</Text>
+                <Text style={styles.peripheralText}>
+                    <Text style={styles.nominator}>Geräte Name: </Text>{item.name === null ? 'Unbekannt' : item.name}
+                </Text>
+                <Text style={styles.peripheralText}>
+                    <Text style={styles.nominator}>Adresse: </Text>{item.id}
+                </Text>
             </View>
         </Pressable>
     );
 }
 
+
+
 export default DeviceListItem;
+
+const styles = StyleSheet.create({
+    devicePressable: {
+        fontFamily: 'Roboto',
+        color: Colors.Grey,
+        backgroundColor: Colors.Background,
+        borderBottomWidth: 1,
+        padding: 8,
+        alignItems: 'stretch',
+    },
+    selectedItem: {
+        fontFamily: 'Roboto',
+        color: Colors.Active,
+        backgroundColor: Colors.Yellow,
+        borderBottomWidth: 1,
+        padding: 5,
+    },
+    peripheralText: {
+        color: Colors.Grey
+    },
+    nominator: {
+        fontFamily: 'Roboto',
+        fontWeight: 'bold',
+    }
+});
